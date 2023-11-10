a002e867b7b6a73f7fc27c754c7d790a9b7f2d47
wynssimon <101094646+wynssimon@users.noreply.github.com>
2023-11-10 17:39:56 +0100

    nu echt laatste

--- post-demo.php
+++ post-demo.php
@@ -4,14 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Post resultaat</title>
-  <link rel="stylesheet" href="./styles/style.css" />
-  <link rel="shortcut icon" href="./img/portfolio.png" type="image/x-icon" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@300;400;500;600;700&display=swap"
-      rel="stylesheet"
-    />
 </head>
 <body>
   <h1>Je post data</h1>
