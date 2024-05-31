3793d9d2a1bb0a79c4d5cdd74ed69216e836b190
Simon Wyns <wynssimonw@gmail.com>
2024-05-31 22:49:06 +0200

    nieuwe fotos's, betere mapstructuur

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -3,11 +3,11 @@ function hamb() {
   let navigatie = document.querySelector(".navigatie");
   //.split("/") splits bij de slashen en de .pop selecteert het laatste element
   var fileName = hamburger.src.split("/").pop();
-  if (fileName === "hamburger.png") {
-    hamburger.src = "/img/close.png";
+  if (fileName === "icons/hamburger.png") {
+    hamburger.src = "/img/icons/close.png";
     navigatie.style.display = "block";
   } else {
-    hamburger.src = "/img/hamburger.png";
+    hamburger.src = "/img/icons/hamburger.png";
     navigatie.style.display = "none";
   }
 }
